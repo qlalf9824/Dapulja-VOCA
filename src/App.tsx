05a6e943b092fcee42abe0618266c1c1fa061028
gayoung.ha <qlalf9824@gmail.com>
2022-05-06 10:40:33 +0900

    add title Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 
 import Home from './Home'
 import QuizSession from './QuizSession'
 import WordList from './WordList'
 
+const Header = {
+  textDecoration: 'none',
+  color: 'inherit'
+}
+
 function App() {
   return (
     <section style={{ textAlign: 'center' }}>
-      <h1>다풀자 영단어</h1>
+      <Link to='/' style={Header}>
+        <h1>다풀자 영단어</h1>
+      </Link>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='quiz' element={<QuizSession />} />
